d31dc3c0472099a5ff02a0a403d997e152d50dcd
Balapriyan B <131746104+BalaPriyan@users.noreply.github.com>
2023-12-01 22:48:50 +0530

    Update info.py

--- info.py
+++ info.py
@@ -2,8 +2,8 @@ from os import environ
 
 API_ID = int(environ.get('API_ID', '9830058'))
 API_HASH = environ.get('API_HASH', '908e8bff7c8f6ecd5d0ab989f78134fc')
-BOT_TOKEN = environ.get('BOT_TOKEN', '6471679179:AAGZRVM4gL_F4p5mili_zgR11a7LNXgJDpw')
+BOT_TOKEN = environ.get('BOT_TOKEN', '6140044105:AAFwZAcD9QfYfEYTiEaeLwKu44gX0LgzZwo')
 FILE_CAPTION = environ.get('FILE_CAPTION', '<code>{file_name}</code>')
-OWNER = int(environ.get('OWNER', '6616684260'))
-PRIVATE_BOT = bool(environ.get('PRIVATE_BOT', False))
+OWNER = int(environ.get('OWNER', '6675988213'))
+PRIVATE_BOT = bool(environ.get('PRIVATE_BOT', True))
 
